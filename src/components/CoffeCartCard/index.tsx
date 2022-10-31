f50d13675c6a373aa825672c58c78768c2e9f6ba
cdfortes <cdfortes@gmail.com>
2022-10-31 16:34:22 -0100

    CoffeeCartCard begin

---
+++ src/components/CoffeCartCard/index.tsx
@@ -0,0 +1,50 @@
+import { ShopItem } from '../../context/ShopContext'
+import { useShop } from '../../hooks/useShop'
+
+interface CoffeeCartCardProps {
+  coffee: ShopItem
+}
+export function CoffeeCartCard({ coffee }: CoffeeCartCardProps) {
+  const { changeCartItemQuantity, removeCartItem } = useShop()
+
+  function handleIncrease() {
+    changeCartItemQuantity(coffee.id, 'increase')
+  }
+
+  function handleDecrease() {
+    changeCartItemQuantity(coffee.id, 'decrease')
+  }
+
+  function handleRemove() {
+    removeCartItem(coffee.id)
+  }
+
+  const coffeeTotal = coffee.price * coffee.quantity
+
+  const formattedPrice = formatMoney(coffeeTotal)
+
+  return (
+    <CoffeeCartCardContainer>
+      <div>
+        <img src={`/coffees/${coffee.photo}`} />
+        <div>
+          <RegularText color="subtitle">{coffee.name}</RegularText>
+          <ActionsContainer>
+            <QuantityInput
+              onIncrease={handleIncrease}
+              onDecrease={handleDecrease}
+              quantity={coffee.quantity}
+              size="small"
+            />
+            <RemoveButton type="button" onClick={handleRemove}>
+              <Trash size={16} />
+              REMOVER
+            </RemoveButton>
+          </ActionsContainer>
+        </div>
+      </div>
+
+      <p>R$ {formattedPrice}</p>
+    </CoffeeCartCardContainer>
+  )
+}
